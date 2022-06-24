07e5716194295f08a8db0a4b7c47a2557ca94c68
Troy-MA <57745873+Troy-MA@users.noreply.github.com>
2022-06-24 10:18:19 +0200

    golden fleece server init

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const userRouter = require("./routes/user");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const DB =
-  "mongodb+srv://cluster009:sFuyFfC0TBZg94Xb@cluster0.oukyvkw.mongodb.net/?retryWrites=true&w=majority";
-
+  "mongodb+srv://Troy-MA-GF:P3dioMYMQV8VGLgs@goldenfleece.nr5xumy.mongodb.net/?retryWrites=true&w=majority";
+  
 // middleware
 app.use(express.json());
 app.use(authRouter);
